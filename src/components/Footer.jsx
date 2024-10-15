7dbaec85da484fdbf3d73eabb7e725f632e8a4ad
rimfat1311 <simontimnan@gmail.com>
2024-10-15 07:18:46 -0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
           <a href="#contact" className="hover:text-white transition duration-300">Contact me</a>
         </div>
         <div className="flex justify-center space-x-8 mb-4">
-          <a href="https://www.linkedin.com/in/rimfat-timnan-simon" target="_blank" rel="noopener noreferrer" className="text-2xl hover:text-white transition duration-300">
+          <a href='https://www.linkedin.com/in/rimfat-timnan-088a022bb/' target="_blank" rel="noopener noreferrer" className="text-2xl hover:text-white transition duration-300">
             <FaLinkedin />
           </a>
           <a href="https://github.com/rimfat-timnan-simon" target="_blank" rel="noopener noreferrer" className="text-2xl hover:text-white transition duration-300">
